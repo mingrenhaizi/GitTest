72beaff4f1adf2eca4e83cd57c5ea97c6a766b92
mingrenhaizi <woshihaode@qq.com>
2019-10-06 12:28:50 +0800

    git 分支测试1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        //分支测试！   bra1
+        System.out.println("你好");
+
+
     }
 }
